cb5ca1f0bc2312f17d4fca8846a7984d0792f92b
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-07 08:41:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,9 +3,9 @@ class Transfer
   attr_accessor :balance, :status
   attr_reader :name
 
-  def initialize(name, status = "pending", balance)
+  def initialize(name, status = "pending", balance = "1000")
     @name = name
-    @balance = 1000
+    @balance = balance
     @status = status
   end
 
